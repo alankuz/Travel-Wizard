3fedf4b228d34739da61a50ebb6229714557b32e
tylermorrisford <tylerford@gmail,com>
2019-08-21 22:02:30 -0600

    fixed slideshow and images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,21 @@
+$(document).ready(function(){
+    $('.carousel').carousel(
+    {
+      dist: 0,
+      padding: 0,
+      fullWidth: true,
+      indicators: true,
+      duration: 100,
+    }
+    );
+  });
+  
+  autoplay()   
+  function autoplay() {
+      $('.carousel').carousel('next');
+      setTimeout(autoplay, 4500);
+  }
+
 var countrylist = [
 
     country = {
